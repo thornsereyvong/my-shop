b298439e1adc3cd21c0a747f5bf68dab6efc1122
Sereyvong-Thorn <sereyvong98@gmail.com>
2017-02-01 17:27:27 +0700

    vxvxvsdvsf

--- app.php
+++ app.php
@@ -3,7 +3,8 @@
 	$server = "http://localhost/we-shop/";
 	include_once 'connection/DBConnection.php';
 	
-	$dbh = (new DBConnection('localhost', 'temp_pos', 'root', ''))->getConnection();
+	$dbh = new DBConnection('localhost', 'temp_pos', 'root', '');
+	$dbh = $dbh->getConnection();
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
 	// 123456789
